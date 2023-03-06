1453c964ec9fbd78cadc7891657a58e87c26a23f
JaiGarg01 <jaigarg850@gmail.com>
2023-03-06 20:40:00 +0530

    Added data to atlas

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose'); 
 
 const EventSchema = mongoose.Schema({
-    Title : String 
-
-
-
+    id : Number , 
+    name : String , 
+    date : Date , 
+    location : String, 
+    description : String, 
+    image : String , 
+    organizer : {name : String, email : String , phone : String}, 
+    category : String, 
+    price : Number 
 });
 
 const Event = mongoose.model('Event' , EventSchema); 
