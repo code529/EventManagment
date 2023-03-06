04d9f3b77374b82046459c16b187d25ba9bcc102
ArnavJyo <arnav.jyotshi@gmail.com>
2023-03-06 18:02:34 +0530

    database setup done

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ const app = express();
 const userRouter = require("./src/routes/userRoutes");
 const eventRouter = require("./src/routes/userRoutes");
 
-const DB = process.env.DB;
-const PORT = process.env.PORT;
-
-mongoose
-  .connect(DB)
-  .then(() => console.log("Connect to Database"))
-  .catch((err) => console.log(err));
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,7 +16,4 @@ app.use(express.static("./src/assets"));
 app.use("/", require("./src/routes"));
 app.use("/api/v1/users/", userRouter);
 app.use("/api/v1/events", eventRouter);
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
+module.exports = app;
