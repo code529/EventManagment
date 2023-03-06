9f555da62b0aab2ed70fe14df5bc30b81a8d0802
ArnavJyo <arnav.jyotshi@gmail.com>
2023-03-06 11:46:35 +0530

    http logger morgan added

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const fs = require("fs");
-const bodyParser = require('body-parser'); 
-const mongoose = require('mongoose');
-require('dotenv').config('./.env'); 
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config("./.env");
 const app = express();
 
-const userRouter = require('./src/routes/userRoutes'); 
-const eventRouter = require('./src/routes/userRoutes'); 
+const userRouter = require("./src/routes/userRoutes");
+const eventRouter = require("./src/routes/userRoutes");
 
-const DB = process.env.DB; 
-const PORT = process.env.PORT; 
+const DB = process.env.DB;
+const PORT = process.env.PORT;
 
-mongoose.connect(DB)
-  .then(()=>console.log("Connect to Database"))
-  .catch((err)=>console.log(err)); 
+mongoose
+  .connect(DB)
+  .then(() => console.log("Connect to Database"))
+  .catch((err) => console.log(err));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./src/assets"));
+app.use("/", require("./src/routes"));
+app.use("/api/v1/users/", userRouter);
+app.use("/api/v1/events", eventRouter);
 
-
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended : true})); 
-app.use(express.static('./src/assets'));
-app.use('/' , require('./src/routes')); 
-app.use('/api/v1/users/', userRouter); 
-app.use('/api/v1/events' , eventRouter); 
-
-
-app.listen(PORT , () => {
+app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
