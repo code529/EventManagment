cff079ef238074fc8f21ecf3658e9d3266acacc3
ArnavJyo <arnav.jyotshi@gmail.com>
2023-03-17 21:06:36 +0530

    user page funcs and review model set

---
+++ src/models/reviewModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Event = require("./eventModel");
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    review: {
+      type: String,
+      required: [true, "Review cant be empty"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    event: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a event"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+reviewSchema.pre("/^find/", function (next) {
+  this.populate({
+    path: "event",
+    select: "name",
+  }).populate({
+    path: "user",
+    select: "name photo",
+  });
+  next();
+});
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
